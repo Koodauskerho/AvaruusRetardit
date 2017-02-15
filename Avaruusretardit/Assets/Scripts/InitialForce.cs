3f05c01782e0b36e5bcc96ab89efa3bbc834d2eb
Sauli Anto <sauli.anto@gmail.com>
2017-02-15 23:17:02 +0200

    Add moon and better gravity

---
+++ Avaruusretardit/Assets/Scripts/InitialForce.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitialForce : MonoBehaviour {
+
+    public Vector3 force;
+
+	// Use this for initialization
+	void Start () {
+        gameObject.GetComponent<Rigidbody>().velocity = force;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
