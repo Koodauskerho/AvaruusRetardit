c7fc9c649040090a12008e359c097ddba06f3588
Anssi Varjonen <anssi.varjonen@student.tut.fi>
2017-02-16 02:45:32 +0200

    Basic flightcontrols added

---
+++ Avaruusretardit/Assets/Scripts/BasicControls.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicControls : MonoBehaviour {
+    public float speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Rigidbody body = GetComponent<Rigidbody>();
+        float verticalmove = Input.GetAxis("Vertical") / 2;
+        float horizontalmove = Input.GetAxis("Horizontal") / 2;
+        float forwardmove = Input.GetAxis("Forward") * speed;
+        float rotation = Input.GetAxis("Rotate") / 2;
+        
+        body.AddRelativeForce(0.0f, 0.0f, forwardmove);
+
+        body.AddRelativeTorque(0.0f, 0.0f, rotation); 
+        body.AddRelativeTorque(0.0f, horizontalmove, 0.0f);
+        body.AddRelativeTorque(verticalmove, 0.0f, 0.0f);
+	}
+}
